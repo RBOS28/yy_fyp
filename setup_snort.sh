013186d7214ba6dbcee88f55501e4d49fe434bf9
robert <yaoyy13@gmail.com>
2023-11-25 01:45:30 +0800

    configure the snort stuff

--- setup_snort.sh
+++ setup_snort.sh
@@ -1,42 +1,35 @@
-# Install Snort
-apt install snort -y
+# Install Snort 
+apt install snort barnyard2 mysql-server apache2 php libapache2-mod-php php-mysql -y
 
-# Configure Snort rules and variables
-cat > /etc/snort/snort.conf <<EOF
-# Rule and decoder configuration
-config disable_decode_alerts
-config disable_tcpopt_experimental_alerts
-config disable_tcpopt_obsolete_alerts
-config disable_tcpopt_ttcp_alerts
-config disable_tcpopt_alerts  
+# Configure MySQL database
+mysql -e "CREATE DATABASE snort;"
+mysql -e "CREATE USER 'snort'@'localhost' IDENTIFIED BY 'password';"  
+mysql -e "GRANT ALL ON snort.* TO 'snort'@'localhost';"
 
-# Home network subnet
-ipvar HOME_NET 192.168.1.0/24  
-
-# DNS Servers 
-ipvar DNS_SERVERS 192.168.1.1 
-
-# Web Servers
-ipvar HTTP_SERVERS 192.168.1.100 
-
-# Rate Limiting 
-config event_filter: rate_filter snort_decoder 5, seconds 5, sid 1  
-
-# Include custom rules  
-include ./rules/local.rules
+# Configure Barnyard2
+cat > /etc/snort/barnyard2.conf << EOF
+config daemon
+config interface: eth0
+config logdir: /var/log/snort
+output database: log, mysql, user=snort password=password dbname=snort host=localhost  
 EOF
 
-# Create directory for custom rules
-mkdir /etc/snort/rules
-
-# Custom rules file
-cat >> /etc/snort/rules/local.rules <<EOF
-# Alert on outbound SSH brute force attempts
-alert tcp \$HOME_NET any -> any 22 (msg:"SSH brute force attempt"; \ 
-   flow:to_server,established; threshold: type threshold, track by_src, count 5, seconds 60; sid:10000001;)
+# Configure Snort rules
+cat > /etc/snort/snort.conf << EOF
+ipvar HOME_NET 192.168.20.0/24
+rulepath ./rules
+include classification.config
+include reference.config
+include $RULE_PATH/snortrules-snapshot-29131.tar
+include $RULE_PATH/app-detect.rules
 EOF
 
-# Set interface and enable service 
-sed -i 's/^# config interface:.*/config interface: eth0/' /etc/snort/snort.conf
-systemctl enable snort
-systemctl start snort
+# Pull community rules
+wget https://www.snort.org/rules/snortrules-snapshot-29131.tar?o=f -O /etc/snort/rules/snortrules-snapshot-29131.tar
+
+# Create IPS drop rules  
+echo "drop tcp $HOME_NET any -> $EXTERNAL_NET 22 (msg: \"Block SSH Bruteforce\"; sid:10000001;)" > /etc/snort/rules/local.rules  
+
+# Start services
+systemctl enable mysql barnyard2 snort apache2
+systemctl start mysql barnyard2 snort apache2
