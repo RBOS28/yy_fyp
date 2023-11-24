72a6b7f3ac22c38f406daf70273d8bf3fbeaf3c2
robert <yaoyy13@gmail.com>
2023-11-25 01:43:20 +0800

    openvpn stuff

--- setup_openvpn.sh
+++ setup_openvpn.sh
@@ -1,50 +1,40 @@
-# Install OpenVPN 
+# Install OpenVPN
 apt install openvpn easy-rsa -y
 
-# Generate PKI keys & certificates
+# Generate keys & certificates
 make-cadir /etc/openvpn/keys
 cd /etc/openvpn/keys
 openvpn --genkey --secret ta.key
-openvpn --csr server ../server.csr
-openvpn --build-ca --batch
-openvpn --dh dh.pem
-openvpn --build-server-full server nopass
-openvpn --build-client-full client1 nopass 
+openvpn --csr server server.csr 
+openvpn --build-ca  
+openvpn --build-key-server server
+openvpn --build-dh
+openvpn --build-client-full client1
 
-# Server Config
+# Server config  
 cat > /etc/openvpn/server.conf <<EOF
 port 1194
 proto udp
 dev tun
 ca /etc/openvpn/keys/ca.crt
-cert /etc/openvpn/keys/server.crt  
+cert /etc/openvpn/keys/server.crt
 key /etc/openvpn/keys/server.key
-dh /etc/openvpn/keys/dh.pem 
-tls-auth /etc/openvpn/keys/ta.key
+dh /etc/openvpn/keys/dh.pem  
+tls-auth /etc/openvpn/keys/ta.key 0
 cipher AES-256-GCM
-auth SHA512
-ifconfig 10.9.0.1 255.255.255.0
-push "redirect-gateway def1 bypass-dhcp" 
-client-to-client
-duplicate-cn
+auth SHA256
+topology subnet
+server 10.8.0.0 255.255.255.0
+ifconfig-pool-persist ipp.txt  
+
+push "redirect-gateway def1 bypass-dhcp"
+push "dhcp-option DNS 8.8.8.8"
 keepalive 10 120
-comp-lzo
-user openvpn
-group openvpn
+explicit-exit-notify 1
 persist-key
 persist-tun
-verb 3
+status /var/log/openvpn-status.log
+log /var/log/openvpn.log
+verb 4
+mute 20
 EOF
-
-# IPtables Rules  
-iptables -t nat -A POSTROUTING -s 10.9.0.0/24 -o eth0 -j MASQUERADE
-iptables -A FORWARD -m state --state RELATED,ESTABLISHED -j ACCEPT 
-iptables -A FORWARD -s 10.9.0.0/24 -j ACCEPT
-
-# Save IPtables Rules
-iptables-save > /etc/iptables.rules
-echo 'net.ipv4.ip_forward=1' >> /etc/sysctl.conf
-
-# Start OpenVPN
-systemctl start openvpn
-systemctl enable openvpn
